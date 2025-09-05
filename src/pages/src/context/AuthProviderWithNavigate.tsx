f2940af197eef2e3418a0269746a7abf674869c3
Djalil920 <djalilkingdz@gmail.com>
2025-09-05 07:42:46 +0100

    wip: sa

--- src/pages/src/context/AuthProviderWithNavigate.tsx
+++
@@ -1,8 +0,0 @@
-// src/context/AuthProviderWithNavigate.tsx
-import { useNavigate } from 'react-router-dom';
-import { AuthProvider } from './AuthContext';
-
-export const AuthProviderWithNavigate = ({ children }: { children: React.ReactNode }) => {
-  const navigate = useNavigate();
-  return <AuthProvider navigate={navigate}>{children}</AuthProvider>;
-};
